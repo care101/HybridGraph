718debc0950d35cf50ece2a78cd6811afbbca2b4
HybridGraph <wangzhiganglab@gmail.com>
2015-10-20 19:28:02 -0400

    Update GraphRecordContextInterface.java

--- src/java/org/apache/hama/myhama/util/GraphRecordContextInterface.java
+++ src/java/org/apache/hama/myhama/util/GraphRecordContextInterface.java
@@ -1,6 +1,5 @@
 /**
  * Termite System
- * NEU SoftLab 401
  * copyright 2012-2010
  */
 package org.apache.hama.myhama.util;
@@ -9,7 +8,7 @@ package org.apache.hama.myhama.util;
  * GraphRecordContextInterface.
  * This class defines operations about one {@link GraphRecord}.
  * 
- * @author WangZhigang
+ * @author 
  * @version 0.1
  */
 public interface GraphRecordContextInterface {
